b8b02dbc8788dcd11c58caff5180d1a4aafd7294
Phil Lackey <phillackey@ip-192-168-1-123.ec2.internal>
2021-04-28 12:54:38 -0500

    Fresh Laravel project with Dusk and basic tests setup, Breeze with Tailwind, and Sail docker container setup on port 3307

---
+++ README.md
@@ -0,0 +1,27 @@
+# Stars Integration Testing
+
+This project uses Laravel Sail instead of a local PHP/MySQL environment or Valet.
+
+Renaming the `.env.example` file to `.env` should get you the right DB host name and correct ports.
+
+## Running Sail
+
+You can get the docker image up and running by entering this command in the root of the directory:
+`./vendor/bin/sail up`
+
+To shut down the docker image:
+`./vendor/bin/sail down`
+
+## View project
+
+Once Sail is running, the project can be viewed at just `localhost`
+
+## View Database
+
+Connect to the db while Sail is running with the default Sail credentials:
+`username: sail`
+`password: password`
+
+## Run Integration Tests
+
+`php artisan dusk`
---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+     /** @test */
+     public function a_user_can_click_the_login_page_starting_from_home_page() {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel')
+                    ->assertSee('Laracasts')
+                    ->clickLink('Log in')
+                    ->waitUntilMissing('#authentication-links')
+                    ->assertSee('Email')
+                    ->assertSee('Password');
+                    
+        });
+    }
+
+    /** @test */
+    public function a_user_can_view_the_login_page() {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                    ->assertSee('Email')
+                    ->assertSee('Password')
+                    ->assertSee('Remember me')
+                    ->assertSee('Forgot your password?')
+                    ->assertSee('LOG IN');
+        });
+    }
+
+    /** @test for External site */
+    public function check_laravel_vapor() {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('https://vapor.laravel.com/')
+                    ->assertSee('Incredible scale without breaking a sweat.');
+        });
+    }
+}
